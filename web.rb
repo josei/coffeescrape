6b22fc19fbe74247582379d26253f780ddf30d13
jose <joseignacio.fernandez@gmail.com>
2012-02-22 13:22:28 +0100

    Check to isolate error

--- web.rb
+++ web.rb
@@ -7,12 +7,9 @@ get '/' do
   haml :index
 end
 
-get '/hi/:uri' do
-  "hi#{params[:uri]}"
-end
-
 get '/proxy/:uri' do
-  RestClient.get params[:uri], :content_type=>'text/html'
+  #RestClient.get params[:uri], :content_type=>'text/html'
+  params[:uri]
 end
 
 helpers do
