a5a6e2cfffca5664cb6d53e4d9fbc7d9ad426ca5
jose <joseignacio.fernandez@gmail.com>
2012-02-22 13:08:24 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source :rubygems
+gem 'sinatra'
+gem 'thin'
+gem 'haml'
+gem 'rest-client'
---
+++ web.rb
@@ -0,0 +1,61 @@
+# -*- encoding: utf-8 -*-
+require 'sinatra'
+require 'rest_client'
+
+get '/' do
+  haml :index
+end
+
+get '/proxy/:uri' do
+  RestClient.get params[:uri], :content_type=>'text/html'
+end
+
+helpers do
+  def base_js
+    <<-EOS
+      jQuery(function($) {
+        $("input#run").click(function() {
+          $("pre#result").text("");
+          try {
+            eval(editor.getValue());
+          } catch(err) {
+            alert("Error:\\n" + err.message);
+          }
+        });
+        
+        $(document).bind('keydown', 'alt+r', function() { $("input#run").click(); });
+
+        function get(real_url, callback) {
+          url = "/proxy/" + encodeURIComponent(real_url);
+          $.get(url, callback);
+        }
+        
+        function println(text) {
+          $("pre#result").text($("pre#result").text()+text+"\\n");
+        }
+        
+      });
+    EOS
+  end
+
+  def code_mirror_js
+    <<-EOS
+      editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+        mode: 'javascript',
+        lineNumbers: true,
+        extraKeys: { 'Alt-R': function() { $("input#run").click(); } },
+        matchBrackets: true
+      });
+    EOS
+  end
+  
+  def example
+    <<-EOS
+get("http://engadget.com", function(result) {
+  $(result).find("h4.post_title a").each(function() {
+    println($(this).text());
+  });
+});
+    EOS
+  end
+end
